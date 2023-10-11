56ca71453ed8bcd6bbdb9326dcb3107b7970a6bd
Blake <cblakequ@gmail.com>
2023-10-11 14:27:40 -0400

    Updated movement
    Added spin ability

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,5 +7,9 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+		if (Input.GetKey(KeyCode.Space))
+		{
+            transform.Rotate(new Vector3(0, 0, Time.deltaTime * 10f));
+		}
     }
 }
