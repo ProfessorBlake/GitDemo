2d2b5c2340d4a5f23e289187a575df0d722577d5
Blake <cblakequ@gmail.com>
2023-10-11 14:09:52 -0400

    Movement
    Added movement code to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
     }
 }
